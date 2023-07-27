ac004766d7ade3abe70c23cb8005a33f6400f4de
HafsaSarker <hafsahaqs@gmail.com>
2023-07-27 15:15:20 -0400

    modify zod schema and insert into db

--- src/pages/AddCreator.tsx
+++ src/pages/AddCreator.tsx
@@ -4,7 +4,13 @@ import { FormData, createSchema, submitNewCreator } from "../utils/createForm"
 import { BsInstagram, BsTwitter, BsYoutube } from 'react-icons/bs'
 
 function AddCreator() {
-  const { register, handleSubmit, formState:{ errors } } = useForm<FormData>({resolver: zodResolver(createSchema)})
+  const win: Window = window;
+
+  const { register, handleSubmit, formState:{ errors, isSubmitting, isSubmitSuccessful } } = useForm<FormData>({resolver: zodResolver(createSchema)})
+
+  if(isSubmitSuccessful){
+    win.location = "/";
+  }
 
   return (
     <div className="flex flex-col place-items-center w-full bg-gradient-to-b from-indigo-300 to-red-400 py-10 text-white font-medium">
@@ -17,38 +23,41 @@ function AddCreator() {
           <input 
             type="text" 
             {...register("name")}
+            disabled={isSubmitting}
           />  
+          {errors.name && <span className="error_msg">{errors.name.message}</span>}
         </label>
-        {errors.name && <span className="error_msg">{errors.name.message}</span>}
 
         <label>
         Image:
         <span className="sub_label">Provide a link to an image of your creator. Be sure to include the http://</span>
           <input 
             type="text" 
-            required
             {...register("imgUrl")}
+            disabled={isSubmitting}
           />
+          {errors.imgUrl && <span className="error_msg">{errors.imgUrl.message}</span>}
         </label>
-        {errors.imgUrl && <span className="error_msg">{errors.imgUrl.message}</span>}
+        
 
         <label>
         Description:
         <span className="sub_label">Provide a description of the creator.What makes them interesting?</span>
           <input 
             type="text" 
-            required
             {...register("description")}
+            disabled={isSubmitting}
           />
+          {errors.description && <span className="error_msg">{errors.description.message}</span>}
         </label>
-        {errors.description && <span className="error_msg">{errors.description.message}</span>}
+        
 
         <div className="flex flex-col text-start">
           <h2 className="text-xl text-indigo-500 font-semibold">SOCIAL MEDIA LINKS</h2>
           <span className="sub_label">Provide at least one of the creator's social media links.</span>
           {(errors.ytLink || errors.igLink || errors.twLink) && 
             <span className="error_msg">
-              No social media link provided
+              Please provide a valid social media link
             </span>}
         </div>
         
@@ -58,6 +67,7 @@ function AddCreator() {
             <input 
               type="text"
               {...register("ytLink")}
+              disabled={isSubmitting}
             />
           </label>
 
@@ -67,6 +77,7 @@ function AddCreator() {
             <input 
               type="text"
               {...register("twLink")}
+              disabled={isSubmitting}
             />
           </label>
 
@@ -76,10 +87,16 @@ function AddCreator() {
             <input 
               type="text"
               {...register("igLink")}
+              disabled={isSubmitting}
             />
           </label>
 
-        <button className="bg-indigo-500 py-2 w-full rounded-md" type="submit">Create</button>
+        <button 
+          className="bg-indigo-500 py-2 w-full rounded-md" 
+          type="submit" 
+          disabled={isSubmitting}>
+            Create
+        </button>
       </form>
     </div>
   )
--- src/utils/createForm.ts
+++ src/utils/createForm.ts
@@ -1,27 +1,39 @@
 import { z, ZodType } from "zod"
+import { SubmitHandler } from "react-hook-form"
+import { supabase } from "../Client";
 
 export type FormData = {
     name: string;
     imgUrl: string;
     description: string;
-    ytLink?: string;
-    twLink?: string;
-    igLink?: string;
+    ytLink?: string | null | undefined;
+    twLink?: string | null | undefined;
+    igLink?: string | null | undefined;
 }
 
 export const createSchema:ZodType<FormData> = z.object({
-    name: z.string().min(2).max(30),
-    imgUrl: z.string(),
-    description: z.string(),
-    ytLink: z.string().optional(),
-    twLink: z.string().optional(),
-    igLink: z.string().optional(),
+    name: z.string().min(2, {message: 'Please enter a name'}).max(30),
+    imgUrl: z.string().url({message: 'Please provide a valid link'}),
+    description: z.string().min(2, {message: 'Description required'}),
+    ytLink: z.union([z.string().url().nullish(), z.literal("")]),
+    twLink: z.union([z.string().url().nullish(), z.literal("")]),
+    igLink: z.union([z.string().url().nullish(), z.literal("")]),
 })
 .refine((data) => data.ytLink || data.igLink || data.twLink , {
     message: 'Provide at least one social media link',
     path: ["ytLink", "igLink", "twLink"]
 })
 
-export const submitNewCreator = (data:FormData) => {
-    console.log(data);  
+export const submitNewCreator: SubmitHandler<FormData> = async(data) => {
+    await supabase
+        .from('creators')
+        .insert({
+            name: data.name,
+            description: data.description,
+            imageURL: data.imgUrl,
+            ytLink: data.ytLink,
+            twLink: data.twLink,
+            instaLink: data.igLink
+        })
+        .select()
 }
